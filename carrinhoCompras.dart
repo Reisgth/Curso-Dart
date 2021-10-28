28a1af921f8f65f55de9f5a54163a00999003a39
Gustavo Reis <reisgth@gmail.com>
2021-10-28 16:42:28 -0300

    func adicionarProdutos

--- carrinhoCompras.dart
+++ carrinhoCompras.dart
@@ -1,11 +1,25 @@
 import 'dart:io';
 
-main() {
-  List<String> itens = [];
-  
+List<String> itens = [];
+
+adicionaItens() {
   while(true) {
-    print("BEM VINDO AS COMPRAS");
-    print("1 - ADICIONAR ITEMS AO CARRINHO");
+    print("DIGITE O ITEM A ADICIONAR NO CARRINHO OU SAIR PARA RETORNAR AO MENU: ");
+    String item = stdin.readLineSync().toString();
+    
+    if(item == "SAIR" || item == "sair") {
+        return main;
+      } // Volta para o menu
+
+    itens.add(item);
+    print("\x1B[2J\x1B[0;0H");
+  }
+}
+main() {
+    
+  while(true) {
+    print("\n=====BEM VINDO AS COMPRAS=====\n");
+    print("1 - ADICIONAR ITENS AO CARRINHO");
     print("2 - MOSTRAR OS ITENS DO CARRINHO");
     print("3 - REMOVER ITENS DO CARRINHO");
     print("4 - ENCERRAR COMPRAS");
@@ -13,8 +27,7 @@ main() {
     int option = int.parse(stdin.readLineSync().toString()); //Recebe dado, convert to string e convert to int
 
     if(option == 1) {
-      print("adicionaItem");
-      print("\x1B[2J\x1B[0;0H");
+      adicionaItens();
     } else if(option == 2) {
       print("mostrarCarrinho");
     } else if (option == 3){
