26a448b0dae019da8a42009eac7c74c80c474a29
Gustavo Reis <reisgth@gmail.com>
2021-10-28 14:02:02 -0300

    IMC e renome

---
+++ IMC.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+main() {
+  //Calculo de IMC
+  var altura, peso, IMC, input;
+
+  print("Digite seu peso: ");
+  input = stdin.readLineSync();
+  peso = double.parse(input);
+
+  print("Digite sua altura: ");
+  input = stdin.readLineSync();
+  altura = double.parse(input);
+
+  IMC = peso/(altura*altura);
+
+  print("\nSeu IMC eh de: ");
+  print(IMC);
+ if (IMC < 18.5) {
+    print("\nAbaixo do Peso");
+  } else if (IMC < 25) {
+      print("\nPeso Normal");
+    } else if (IMC < 30) {
+        print("\nSobrepeso");
+      } else if (IMC < 35) {
+          print("\nObesidade1");
+        } else if (IMC < 40) {
+            print("\nObesidade2");
+          } else {
+              print("\nObesidade3");
+            }
+}
