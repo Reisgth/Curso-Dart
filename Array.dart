f3f9b4ce48bcaf7ec97f046664f7593989d68b82
Gustavo Reis <reisgth@gmail.com>
2021-10-28 15:40:06 -0300

    Modificando array

--- Array.dart
+++ Array.dart
@@ -1,20 +1,33 @@
 import 'dart:io';
 
 main() {
+  
   var nomes = [];
   bool isTrue = true;
 
-  while(isTrue) {
+  while (isTrue) {
     print("\nDigite um nome ou sair para finalizar: ");
     String text = stdin.readLineSync().toString();
 
-    if(text == "sair"){
-      print("\nPrograma Finzalido\n");
+    if (text == "sair") {
+      print("Programa Finzalido");
       isTrue = false;
     } else {
       nomes.add(text);
     }
   }
 
-  print("\n$nomes");
+  print("$nomes\n");
+
+  var carros = [];
+
+  carros = ["Gol", "Celta", "Kadett", "Agile"];
+
+  print(carros);
+
+  carros.add("Prisma"); // Adiciona no final do array
+  carros.remove("Gol"); // Procura o valor e remove
+  carros.removeAt(2); // Remove o item em determinada posição
+
+  print(carros);
 }
