5ff77122dc4f42f9ceebef2606cb7b0d7708fce0
Gustavo Reis <reisgth@gmail.com>
2021-10-28 16:53:54 -0300

    func mostrarItens

--- carrinhoCompras.dart
+++ carrinhoCompras.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 List<String> itens = [];
 
 adicionaItens() {
+
+  print("\x1B[2J\x1B[0;0H");
   while(true) {
     print("DIGITE O ITEM A ADICIONAR NO CARRINHO OU SAIR PARA RETORNAR AO MENU: ");
     String item = stdin.readLineSync().toString();
@@ -15,8 +17,24 @@ adicionaItens() {
     print("\x1B[2J\x1B[0;0H");
   }
 }
+
+mostrarItens() {
+  int x;
+  
+  print("\x1B[2J\x1B[0;0H");
+  for(x=0;x<itens.length;x++)
+    print("Item $x - ${itens[x]}");
+
+  print("\nDIGITE SAIR PARA RETORNAR AO MENU");
+  String item = stdin.readLineSync().toString();
+
+  if (item == "SAIR" || item == "sair") {
+    return main;
+  } // Volta para o menu
+}
 main() {
-    
+  
+  print("\x1B[2J\x1B[0;0H");
   while(true) {
     print("\n=====BEM VINDO AS COMPRAS=====\n");
     print("1 - ADICIONAR ITENS AO CARRINHO");
@@ -29,7 +47,7 @@ main() {
     if(option == 1) {
       adicionaItens();
     } else if(option == 2) {
-      print("mostrarCarrinho");
+      mostrarItens();
     } else if (option == 3){
       print("removerItem");
     } else {
