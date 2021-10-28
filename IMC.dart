f41c3a0b6dc90924f0cf12984a13d97ac38bb90c
Gustavo Reis <reisgth@gmail.com>
2021-10-28 14:09:24 -0300

    implementando funcao

--- IMC.dart
+++ IMC.dart
@@ -1,5 +1,32 @@
 import 'dart:io';
 
+calculoIMC(peso, altura) {
+
+  var IMC = peso / (altura * altura);
+
+  return IMC;
+}
+
+mostraIMC(IMC) {
+
+  print("\nSeu IMC eh de: ");
+  print(IMC);
+  
+  if (IMC < 18.5) {
+    print("\nAbaixo do Peso");
+  } else if (IMC < 25) {
+    print("\nPeso Normal");
+  } else if (IMC < 30) {
+    print("\nSobrepeso");
+  } else if (IMC < 35) {
+    print("\nObesidade1");
+  } else if (IMC < 40) {
+    print("\nObesidade2");
+  } else {
+    print("\nObesidade3");
+  }
+}
+
 main() {
   //Calculo de IMC
   var altura, peso, IMC, input;
@@ -12,21 +39,7 @@ main() {
   input = stdin.readLineSync();
   altura = double.parse(input);
 
-  IMC = peso/(altura*altura);
+  IMC = calculoIMC(peso, altura);
 
-  print("\nSeu IMC eh de: ");
-  print(IMC);
- if (IMC < 18.5) {
-    print("\nAbaixo do Peso");
-  } else if (IMC < 25) {
-      print("\nPeso Normal");
-    } else if (IMC < 30) {
-        print("\nSobrepeso");
-      } else if (IMC < 35) {
-          print("\nObesidade1");
-        } else if (IMC < 40) {
-            print("\nObesidade2");
-          } else {
-              print("\nObesidade3");
-            }
+  mostraIMC(IMC);
 }
