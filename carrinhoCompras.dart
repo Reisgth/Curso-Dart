afd3fe721d8afc4ae755b057a8881593b68a2239
Gustavo Reis <reisgth@gmail.com>
2021-10-28 20:55:55 -0300

    func removerItem reparos

--- carrinhoCompras.dart
+++ carrinhoCompras.dart
@@ -5,11 +5,13 @@ List<String> itens = [];
 adicionaItens() {
 
   print("\x1B[2J\x1B[0;0H");
+
   while(true) {
     print("DIGITE O ITEM A ADICIONAR NO CARRINHO OU SAIR PARA RETORNAR AO MENU: ");
     String item = stdin.readLineSync().toString();
     
     if(item == "SAIR" || item == "sair") {
+        print("\x1B[2J\x1B[0;0H");
         return main;
       } // Volta para o menu
 
@@ -20,23 +22,39 @@ adicionaItens() {
 
 mostrarItens() {
   int x;
-  
+
   print("\x1B[2J\x1B[0;0H");
-  for(x=0;x<itens.length;x++)
-    print("Item $x - ${itens[x]}");
 
-  print("\nDIGITE SAIR PARA RETORNAR AO MENU");
-  String item = stdin.readLineSync().toString();
+  if(itens.isNotEmpty){
+    for (x = 0; x < itens.length; x++) print("Item $x - ${itens[x]}");
+  } else print("CARRINHO VAZIO");
+  
+  print("\n");
 
-  if (item == "SAIR" || item == "sair") {
-    return main;
-  } // Volta para o menu
+}
+
+removerItens() {
+
+  while (itens.isNotEmpty) {
+    mostrarItens();
+    print("DIGITE O NUMERO DO ITEM A REMOVER DO CARRINHO OU SAIR PARA RETORNAR AO MENU: ");
+    String text = stdin.readLineSync().toString();
+
+    if (text == "SAIR" || text == "sair") {
+      return main;
+    } // Volta para o menu
+
+    int item = int.parse(text);
+    itens.removeAt(item);
+    print("\x1B[2J\x1B[0;0H");
+  }
+
+  mostrarItens();
 }
 main() {
-  
   print("\x1B[2J\x1B[0;0H");
   while(true) {
-    print("\n=====BEM VINDO AS COMPRAS=====\n");
+    print("=====BEM VINDO AS COMPRAS=====\n");
     print("1 - ADICIONAR ITENS AO CARRINHO");
     print("2 - MOSTRAR OS ITENS DO CARRINHO");
     print("3 - REMOVER ITENS DO CARRINHO");
@@ -49,8 +67,9 @@ main() {
     } else if(option == 2) {
       mostrarItens();
     } else if (option == 3){
-      print("removerItem");
+      removerItens();
     } else {
+      print("\x1B[2J\x1B[0;0H");
       print("=====OBRIGADO PELA PREFERENCIA!=====");
       return 0;
     }
