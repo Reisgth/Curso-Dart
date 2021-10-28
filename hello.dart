4896d1bb773691ae7837813d53db53bd17cc3525
Gustavo Reis <reisgth@gmail.com>
2021-10-28 11:10:15 -0300

    primeiro algoritmo

--- hello.dart
+++ hello.dart
@@ -1,5 +1,15 @@
 main() {
 
-    print("Hello World");
+  var idade;
+
+  print("Digite a sua idade\n");
+
+  idade = 15;
+
+  if(idade < 18) {
+    print("Ele nao eh maior de idade");
+  } else {
+    print("Ele eh maior de idade");
+  }
 
 }
